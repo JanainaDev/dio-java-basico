7fcfd1f2765991fdb132713c982fedb1a37fdb36
Janaina <janaina.reis@fatec.sp.gov.br>
2024-06-06 14:05:44 -0300

    sintaxe básica

--- anatomia-classes/src/MinhaClasse.java
+++ anatomia-classes/src/MinhaClasse.java
@@ -1,6 +1,18 @@
 public class MinhaClasse {
-   public static void main(String[] args) {
-        System.out.print ("Olá turma, sejam bem-vindos!");
-   }
+public static void main(String[] args) {
+    String primeiroNome = "Janaina";
+    String segundoNome = "Pereira";
+
+    String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+    System.out.println(nomeCompleto); // chama a variavel, que por sua vez chamou o método
+
+
+}
+public static String nomeCompleto(String primeiroNome, String segundoNome){
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    //return "Resultado do método ".concat(primeiroNome).concat(" ").concat(segundoNome); é uma outra opção de concatenar
     
+    /*  da pra concatenar com símbolo de + ou usando concat, na linha acima
+    foi usado as 2 formas */
+} 
 }
